e218beafa6b1fc3f3eaf61382d0a256f3c3eb667
Trinh Nguyen <137372274+zTrinh121@users.noreply.github.com>
2025-05-17 00:47:09 +0700

    Remove arrow folder

---
+++ 01-next-rn/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "antd";
+
+export default function Home() {
+  return (
+    <div>hello world nextjs with hoidanit
+      <div>
+        <Button type="primary" > Add new</Button>
+      </div>
+    </div>
+  );
+}
